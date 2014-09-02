ecb69d0905eb7fcf69cbb076f26b581324351f80
alongken2005 <360586201@qq.com>
2014-09-02 23:27:39 +0800

    傅芬芳

--- application/controllers/rp.php
+++ application/controllers/rp.php
@@ -17,15 +17,29 @@ class Rp extends CI_Controller {
 	 * 默认方法
 	 */
 	public function index() {
-		$e = file_get_contents("http://www.juemei.cc/html/list/PiaCdJCbHHaammbaC.html");
+		$furl = "http://www.juemei.cc/html/list/PiaCdJCbHHaammbaC.html";
+		$e = file_get_contents($furl);
 		$sites = $this->config->item('sites');
 		foreach($sites as $k=>$v) {
+			$imgArr = array();
 			$ret = $this->base->get_data('pageUrl', array('siteId'=>$k, 'state'=>0))->result_array();
 			foreach($ret as $row) {
 				$content = file_get_contents($row['pageUrl']);
-				$reg = "/<div id=\"viewimgbox\"><img src=\"(.*?)\".*<div class=\"showpage\"><a href=\"([^<>]+)\">上一组<\/a><em>1<\/em><href=\"(.*?)\">\d{1,2}<\/a>/";
+				$reg = "/<div id=\"viewimgbox\"><img src=\"(.*?)\".*<div class=\"showpage\">(.*?)<\/div>/";
+				//$reg = "/<div id=\"viewimgbox\"><img src=\"(.*?)\".*<div class=\"showpage\"><a href=\"([^<>]+)\">上一组<\/a><em>1<\/em><href=\"(.*?)\">\d{1,2}<\/a>/";
 				preg_match($reg, $content, $match);
+				$imgArr[] = $match[1];
 
+				if(isset($match[2])) {
+					$reg = "/<a href=\'.*?\'>(\d{1,2})<\/a>/";
+					preg_match_all($reg, $match[2], $match2);
+					
+					if($match2) {
+						foreach ($match2 as $value) {
+							
+						}
+					}
+				}
 				debug($match);
 			}
 		}
